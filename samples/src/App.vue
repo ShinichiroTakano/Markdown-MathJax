43a4f4bae443272137635e06e76d5b5414260a9a
takano-shinichiro <shi.takano@signate.co.jp>
2019-07-26 16:07:25 +0900

    makedjsとmathjaxをnode_modules配下に加える

--- samples/src/App.vue
+++ samples/src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
+    <operation-description-area
+      :operationDescription="'aaaaaaaaa'"
+    />
   </div>
 </template>
 
 <script>
-
+import OperationDescriptionArea from './components/OperationDescriptionArea.vue'
 export default {
   name: 'app',
-  components: {}
+  components: {
+   OperationDescriptionArea
+  }
 }
 </script>
 
---
+++ samples/src/components/OperationDescriptionArea.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="content" v-html="compiledMarkdown"></div>
+</template>
+<script>
+import marked from 'marked';
+import aaa from 'mathjax'
+export default {
+  name: 'operation-description',
+  props: {
+    operationDescription: {
+      type: String,
+      required: true
+    }
+  },
+  created() {
+    console.log('aaa', aaa)
+    console.log('MathJax', MathJax)
+  },
+  computed: {
+    compiledMarkdown() {
+      return 'avdvdvdvdv'
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
